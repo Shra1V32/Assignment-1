61c3a66fcca77c4777d45e9583a88adda8ed697e
Nageshwar128 <89348492+Nageshwar128@users.noreply.github.com>
2022-01-22 15:46:22 +0530

    Upload Lab Programs

---
+++ Lab/1.c
@@ -0,0 +1,12 @@
+// Program by Shravan 2451-21-737-113
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char a[100]; // String is declared here as an array.
+    printf("Enter your string: ");
+    gets(a);
+    printf("Your entered string is: ");
+    puts(a);
+    return 0;
+}
