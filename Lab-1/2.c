e3661a568c09b24f82d08e0f913e2c5c9d35c048
Nageshwar128 <namanageshwar@outlook.com>
2022-01-28 09:53:45 +0530

    Move Lab-1 Programs to a folder

---
+++ Lab-1/2.c
@@ -0,0 +1,13 @@
+// Program by Shravan 2451-21-737-113
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char a[100];
+    int x;
+    printf("Enter your string: ");
+    gets(a);
+    x=strlen(a);
+    printf("The number of characters in your string is: %d",x);
+    return 0;
+}
